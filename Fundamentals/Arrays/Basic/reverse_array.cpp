1c187df62fdc438422da49b5200c0299f9f60cd7
Sumanth <sumugowd@gmail.com>
2026-03-05 12:59:54 +0530

    Fixed submodule issue and added array basics

---
+++ Fundamentals/Arrays/Basic/reverse_array.cpp
@@ -0,0 +1,34 @@
+/*
+Problem: Reverse an Array
+Pattern: Two Pointer / STL
+
+Time Complexity: O(N)
+Space Complexity: O(1)
+
+Approach:
+Using C++ STL reverse() to reverse the array in-place.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+void reverseArray(vector<int> &arr);
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    reverseArray(arr);
+
+    for(int i=0;i<n;i++){
+        cout << arr[i]<<" ";
+    }
+    return 0;
+}
+
+void reverseArray(vector<int> &arr) {
+    reverse(arr.begin(),arr.end());
+}
